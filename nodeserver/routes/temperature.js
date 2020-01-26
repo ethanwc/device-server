14d124e847c733af5f308382323a26ebdf126bd2
ethanwc <ethanwc@u.washington.edu>
2020-01-25 18:55:38 -0800

    es5

--- nodeserver/routes/temperature.js
+++ nodeserver/routes/temperature.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var express = require("express");
 
 var router = express.Router();
@@ -7,20 +9,21 @@ var path = require("path");
 var spawn = require("child_process").spawn;
 /* GET temperature listing. */
 
-router.get("/", function(req, res, next) {
+
+router.get("/", function (req, res, next) {
   function runScript() {
     return spawn("python", ["-u", path.join(__dirname, "hello.py")]);
   }
 
   var subprocess = runScript(); // print output of script
 
-  subprocess.stdout.on("data", data => {
+  subprocess.stdout.on("data", function (data) {
     if (!res.headersSent) res.send(data);
   });
-  subprocess.stderr.on("data", data => {
-    console.log(`error:${data}`);
+  subprocess.stderr.on("data", function (data) {
+    console.log("error:".concat(data));
   });
-  subprocess.stderr.on("close", () => {
+  subprocess.stderr.on("close", function () {
     console.log("Closed");
   });
 });
