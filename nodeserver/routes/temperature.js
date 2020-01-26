41cc160b69a8015ebd09f2d4e24cd7d58a1f8d98
ethanwc <ethanwc@u.washington.edu>
2020-01-25 18:31:22 -0800

    converted to es5

--- nodeserver/routes/temperature.js
+++ nodeserver/routes/temperature.js
@@ -1,19 +1,22 @@
 var express = require("express");
-var router = express.Router();
-const path = require("path");
-var spawn = require("child_process").spawn;
 
+var router = express.Router();
+
+var path = require("path");
+
+var spawn = require("child_process").spawn;
 /* GET temperature listing. */
+
 router.get("/", function(req, res, next) {
   function runScript() {
     return spawn("python", ["-u", path.join(__dirname, "hello.py")]);
   }
-  const subprocess = runScript();
-  // print output of script
-  subprocess.stdout.on("data", data => {
-    console.log(`data:${data}`);
+
+  var subprocess = runScript(); // print output of script
+
+  subprocess.stdout.on("data", function(data) {
+    console.log("data:".concat(data));
     res.send(data);
   });
 });
-
 module.exports = router;
