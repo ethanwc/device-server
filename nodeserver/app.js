f1ca85c7f60a6edae4683e704e9eccfa0cb601c7
ethanwc <ethanwc@u.washington.edu>
2020-01-27 00:25:25 -0800

    added led listener

--- nodeserver/app.js
+++ nodeserver/app.js
@@ -5,6 +5,7 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var temperatureRouter = require("./routes/temperature");
+var ledRouter = require("./routes/led");
 
 var app = express();
 
@@ -19,6 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/temperature", temperatureRouter);
+app.use("/led", ledRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ nodeserver/routes/led.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var express = require("express");
+
+var router = express.Router();
+
+var path = require("path");
+
+var spawn = require("child_process").spawn;
+/* GET temperature listing. */
+
+router.post("/", function(req, res, next) {
+  function runScript() {
+    return spawn("python", ["-u", path.join(__dirname, "../led.py")]);
+  }
+
+  var subprocess = runScript(); // print output of script
+
+  subprocess.stdout.on("data", function(data) {
+    if (!res.headersSent) res.send(data);
+  });
+  subprocess.stderr.on("data", function(data) {
+    console.log("error:".concat(data));
+  });
+  subprocess.stderr.on("close", function() {
+    console.log("Closed");
+  });
+});
+module.exports = router;
